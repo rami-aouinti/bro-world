56400b67b6a2bc2a6a2f845f3bacaec5c58964a2
RamiAouinti <rami.aouinti@tkdeutschland.de>
2025-05-30 00:02:34 +0200

    Add authentication

--- server/api/auth/register.post.ts
+++ server/api/auth/register.post.ts
@@ -1,23 +1,16 @@
 import { defineEventHandler, readBody } from 'h3'
 import axios from 'axios'
 
-import https from 'https'
-const httpsAgent = new https.Agent({
-  rejectUnauthorized: false,
-  checkServerIdentity: () => null,
-})
-
 export default defineEventHandler(async (event) => {
   const body = await readBody(event)
 
   const response = await axios.post(
-    'https://bro-world-system.de/api/v1/auth/register',
+    'https://bro-world.org/api/v1/auth/register',
     {
       email: body.email,
       password: body.password,
       repeatPassword: body.repeatPassword,
     },
-    { httpsAgent },
   )
 
   return !!response.data
