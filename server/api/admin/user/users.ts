4b83d9b5df7120252f8530879db307af828b885d
RamiAouinti <rami.aouinti@tkdeutschland.de>
2025-05-31 19:59:51 +0200

    Add stories

--- server/api/admin/user/users.ts
+++ server/api/admin/user/users.ts
@@ -3,8 +3,7 @@ import axios from 'axios'
 
 export default defineEventHandler(async (event) => {
   const session = await requireUserSession(event)
-
-  if (!session || !session.user || !session.user.token) {
+  if (!session || !session.user) {
     throw createError({
       statusCode: 401,
       message: 'Unauthorized',
