58247856817978e259d5b81d641eb11b77357034
RamiAouinti <rami.aouinti@tkdeutschland.de>
2025-06-01 23:43:59 +0200

    Retry

--- components/Auth/Social.vue
+++ components/Auth/Social.vue
@@ -1,27 +1,27 @@
 <template>
   <v-row>
     <v-col cols="2" class="ms-auto d-flex justify-center">
-      <NuxtLink to="/api/auth/github" class="text-decoration-none" style="color: inherit">
+      <NuxtLink href="/api/auth/github" class="text-decoration-none" style="color: inherit">
         <v-icon>mdi-instagram</v-icon>
       </NuxtLink>
     </v-col>
     <v-col cols="2" class="d-flex justify-center">
-      <NuxtLink to="/api/auth/github" class="text-decoration-none" style="color: inherit">
+      <NuxtLink href="/api/auth/github" class="text-decoration-none" style="color: inherit">
         <v-icon>mdi-github-circle</v-icon>
       </NuxtLink>
     </v-col>
     <v-col cols="2" class="d-flex justify-center">
-      <NuxtLink to="/api/auth/facebook" class="text-decoration-none" style="color: inherit">
+      <NuxtLink href="/api/auth/facebook" class="text-decoration-none" style="color: inherit">
         <v-icon>mdi-facebook</v-icon>
       </NuxtLink>
     </v-col>
     <v-col cols="2" class="d-flex justify-center">
-      <NuxtLink to="/api/auth/google" class="text-decoration-none" style="color: inherit">
+      <NuxtLink href="/api/auth/google" class="text-decoration-none" style="color: inherit">
         <v-icon>mdi-google</v-icon>
       </NuxtLink>
     </v-col>
     <v-col cols="2" class="me-auto d-flex justify-center">
-      <NuxtLink to="/api/auth/github" class="text-decoration-none" style="color: inherit">
+      <NuxtLink href="/api/auth/github" class="text-decoration-none" style="color: inherit">
         <v-icon>mdi-linkedin</v-icon>
       </NuxtLink>
     </v-col>
