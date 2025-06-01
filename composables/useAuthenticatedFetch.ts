1098c351a6e07bf896886f9a7074c07701019e2d
RamiAouinti <rami.aouinti@tkdeutschland.de>
2025-06-01 18:34:34 +0200

    Add Timeout for Token

--- composables/useAuthenticatedFetch.ts
+++ composables/useAuthenticatedFetch.ts
@@ -1,9 +1,18 @@
 import axios from 'axios'
-import {useAuthStore} from '@/stores/useAuthStore'
+import { useAuthStore } from '@/stores/useAuthStore'
 
-export async function useAuthenticatedAxios() {
+export async function useAuthenticatedAxios(): Promise<typeof axios> {
   const auth = useAuthStore()
-  const token = auth.getToken
+
+  // Retry loop : attendre jusqu'à 1s (10 x 100ms)
+  let retries = 10
+  let token = auth.getToken
+
+  while ((!token || token.trim() === '') && retries > 0) {
+    await new Promise(resolve => setTimeout(resolve, 100)) // attendre 100ms
+    token = auth.getToken
+    retries--
+  }
 
   return axios.create({
     baseURL: '/',
