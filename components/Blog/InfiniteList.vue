2ced2f4321da7d773a182c1dbd5977c3a128126c
RamiAouinti <rami.aouinti@tkdeutschland.de>
2025-06-04 19:46:37 +0200

    Speed Insights

--- components/Blog/InfiniteList.vue
+++ components/Blog/InfiniteList.vue
@@ -21,8 +21,6 @@ const loadMore = async () => {
   isLoading.value = true
 
   try {
-    await delay(4)
-
     const response = await fetch(`${props.fetchUrl}?page=${page.value}&limit=${props.limit || 5}`)
     const result = await response.json()
 
